d433bda48c74c66797e471db26b2fbf95f317e9e
oat9002 <oat9002@hotmail.com>
2023-12-02 01:27:01 +0700

    🚧

--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine(Day1.Part1());
+        Console.WriteLine(Day1.Part2());
     }
 }
--- answers/Day1.cs
+++ answers/Day1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 
 namespace advent_of_code_2023.Answers
 {
@@ -7,22 +8,40 @@ namespace advent_of_code_2023.Answers
         public static int Part1()
         {
             var input = InputHelper.GetInput(1, 1);
-            var first = -1;
-            var last = -1;
-            var sum = 0;
+            var bag = new ConcurrentBag<int>();
 
-            foreach (var line in input)
+            Parallel.ForEach(input, line =>
             {
-                foreach (var c in line)
+                var first = -1;
+                var last = -1;
+                var length = line.Length;
+
+                for (int i = 0; i < length; i++)
                 {
+                    var c = line[i];
                     try
                     {
                         var cToInt = int.Parse(c.ToString());
 
-                        first = first == -1 ? cToInt : first;
-                        last = cToInt;
+                        first = cToInt;
+                        break;
                     }
-                    catch (Exception)
+                    catch
+                    {
+                        continue;
+                    }
+                }
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    var c = line[i];
+                    try
+                    {
+                        var cToInt = int.Parse(c.ToString());
+
+                        last = cToInt;
+                        break;
+                    }
+                    catch
                     {
                         continue;
                     }
@@ -30,10 +49,79 @@ namespace advent_of_code_2023.Answers
 
                 var numberToAdd = int.Parse($"{first}{last}");
 
-                sum += numberToAdd;
-            }
+                bag.Add(numberToAdd);
+            });
 
-            return sum;
+            return bag.Sum();
+        }
+
+        public static int Part2()
+        {
+            var input = InputHelper.GetInput(1, 2);
+            var bag = new ConcurrentBag<int>();
+            var digitDict = new Dictionary<string, int>()
+            {
+                {"one", 1},
+                {"two", 2},
+                {"three", 3},
+                {"four", 4},
+                {"five", 5},
+                {"six", 6},
+                {"seven", 7},
+                {"eight", 8},
+                {"nine", 9}
+            };
+
+            Parallel.ForEach(input, line =>
+            {
+                var first = -1;
+                var last = -1;
+                var modLine = line;
+
+                foreach (var (key, value) in digitDict)
+                {
+                    modLine = modLine.Replace(key, value.ToString());
+                }
+
+                var length = modLine.Length;
+
+                for (int i = 0; i < length; i++)
+                {
+                    var c = modLine[i];
+                    try
+                    {
+                        var cToInt = int.Parse(c.ToString());
+
+                        first = cToInt;
+                        break;
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    var c = modLine[i];
+                    try
+                    {
+                        var cToInt = int.Parse(c.ToString());
+
+                        last = cToInt;
+                        break;
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+
+                var numberToAdd = int.Parse($"{first}{last}");
+
+                bag.Add(numberToAdd);
+            });
+
+            return bag.Sum();
         }
     }
 }
